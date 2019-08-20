7d1937fbc01fbf687b43af5375588e69c39e1531
Dylan Craig <dylancraig@Dylans-MacBook-Pro.local>
2019-08-20 15:33:21 -0400

    Initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Navbar = ({ icon, title }) => (
+  <nav className='navbar bg-primary'>
+    <h1>
+      <i className={icon} />
+      {' ' + title}
+    </h1>
+    <ul>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+    </ul>
+  </nav>
+);
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github'
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
+
+export default Navbar;
